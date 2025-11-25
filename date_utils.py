9456718f7b6f1039b5eff35a0b362670a6a63f72
scarletttaosj <143381576+scarletttaosj@users.noreply.github.com>
2025-11-25 14:45:44 +0800

    fix
    Fix date format in days_between function.

--- date_utils.py
+++ date_utils.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+
 def days_between(date1, date2):
-  d1 = datetime.strptime(date1, "%Y-%m-%d")
+  # 故意改成错误的格式（一个用斜杠，一个用横杠）
+  d1 = datetime.strptime(date1, "%Y/%m/%d")
   d2 = datetime.strptime(date2, "%Y-%m-%d")
+
+  # 故意去掉 abs,导致结果负数
   return abs((d2 - d1).days)
